9fd574e59e8e23a770bcf5485805fc9a16b05ef6
Hossein Pursultani <hossein@freshbits.io>
2018-01-30 21:32:21 +1100

    feature(runtime): add runtime context facility
    WIP

--- src/runtime.js
+++ src/runtime.js
@@ -3,19 +3,37 @@
  */
 
 import findUp from 'find-up';
+import generateCommandLineUsage from 'command-line-usage';
+import parseCommandLineArgs from 'command-line-args';
 
-/* eslint-disable import/no-dynamic-require */
-const PACKAGE_JSON = require(findUp.sync('package.json'));
-/* eslint-enable import/no-dynamic-require */
 const ATTRIBUTE_NAMES = [
   'name', 'version', 'description', 'author', 'service'];
 
 function readDefaultMetadata() {
-  return new Map(Object.entries(PACKAGE_JSON)
+  /* eslint-disable import/no-dynamic-require */
+  /* eslint-disable global-require */
+  return new Map(Object
+    .entries(require(findUp.sync('package.json')))
     .filter(entry => ATTRIBUTE_NAMES.includes(entry[0])));
+  /* eslint-enable import/no-dynamic-require */
+  /* eslint-enable global-require */
+}
+
+function readDefaultRuntimeOptions() {
+  /* eslint-disable global-require */
+  const result = require('./runtime.options.json');
+  /* eslint-enable global-require */
+  result.forEach((item) => {
+    /* eslint-disable no-eval */
+    item.type = item.type ? eval(item.type) : String;
+    /* eslint-enable no-eval */
+  });
+  return result;
 }
 
 const DEFAULT_METADATA = readDefaultMetadata();
+const DEFAULT_OPTIONS = readDefaultRuntimeOptions();
+const DEFAULT_ENVIRONMENT = 'development';
 
 function setAttribute(source, target, name) {
   let value;
@@ -62,6 +80,52 @@ class PackageMetadata {
 }
 
 class Runtime {
+
+  constructor({ packageMetadata, optionsDefinition, commandArgs }) {
+    this.metadata = packageMetadata || new PackageMetadata();
+    this.options = parseCommandLineArgs(optionsDefinition || DEFAULT_OPTIONS, {
+      argv: commandArgs,
+    });
+    this.environment = process.env.NODE_ENV || this.options.environment || DEFAULT_ENVIRONMENT;
+  }
+
+  get title() {
+    if (!this.cachedTitle) {
+      this.cachedTitle = `${this.metadata.name} - v${this.metadata.version}`;
+    }
+    return this.cachedTitle;
+  }
+
+  get usage() {
+    if (!this.cachedUsage) {
+      this.cachedUsage = generateCommandLineUsage([
+        {
+          header: this.title,
+          content: this.metadata.description || '',
+        }, {
+          header: 'Options',
+          optionList: this.options,
+        },
+      ]);
+    }
+    return this.cachedUsage;
+  }
+
+  printVersion() {
+    if (this.title) {
+      /* eslint-disable no-console */
+      console.error(this.title);
+      /* eslint-enable no-console */
+    }
+  }
+
+  printUsage() {
+    if (this.usage) {
+      /* eslint-disable no-console */
+      console.error(this.usage);
+      /* eslint-enable no-console */
+    }
+  }
 
 }
 
